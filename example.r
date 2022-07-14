04c413d0cbc989720524c40df0ce7a799e021b00
neil <orderllrobbieesquire@gmail.com>
2022-07-14 06:06:56 -0400

    close

--- example.r
+++ example.r
@@ -28,46 +28,56 @@ con <- dbConnect(RPostgres::Postgres(), dbname = db, host=host_db, port=db_port,
 tables <- dbListTables(con) 
 print(con)
 print(tables)
-dydxd <- as.data.frame(dbGetQuery(con, 'SELECT d.asset_pair, dd.dydx_id, dd.trailing_2h_standard_deviation, dd.delta_10min FROM dydx d, dydxd dd where d.id = dd.dydx_id'))
-#df <- data.frame("AssetPair"=as.character(dydxd[[1]]),"ID"=as.numeric(dydxd[[2]]))
-df <- data.frame("T2STD"=as.numeric(dydxd[[3]]),"D10M"=as.numeric(dydxd[[4]]))
-print(df)
+dydxd <- as.data.frame(dbGetQuery(con, 'SELECT d.asset_pair, dd.dydx_id, ((dd.trailing_2h_standard_deviation - dd.trailing_1h_standard_deviation) / dd.trailing_2h_standard_deviation) * 100.0, dd.delta_10min FROM dydx d, dydxd dd where d.id = dd.dydx_id'))
 
-res <- GMM(
-  df,
-  gaussian_comps = 4,
-  dist_mode = "eucl_dist",
-  seed_mode = "random_subset",
-  km_iter = 10,
-  em_iter = 5,
-  verbose = FALSE,
-  var_floor = 1e-10,
-  seed = 1
-)
 
-pr = predict(res, newdata = df)
+for(i in unique(dydxd$asset_pair) %>% sort) {
 
-df$col3 <- pr 
-print(df)
-colnames(df) <- c('T2STD','D10M','Cluster')
+  print(i)
 
-ggplot(df, aes(x=T2STD,y=D10M)) +
-  geom_point(aes(color=factor(Cluster), alpha=0.3)) +
-  xlim(-.10,.10) +
-  ylim(-.10,.10) +
-  theme_tufte() +
-  guides(alpha=FALSE) +
-  scale_color_simpsons() +
-  scale_fill_simpsons() +
-   labs(y="", x="", title="tmp", caption="tmp") +
-  theme(
-    axis.title.x = element_text(color="#e3120b", size=12, face="bold", margin = margin(t = 10, r = 20, b = 0, l = 0)),
-    axis.title.y = element_text(color="#e3120b", size=12, face="bold"),
-    axis.text.x = element_text(angle = 90, vjust = 1)
-    )
+  dydxdf <- filter(dydxd,asset_pair==i)
+  df <- data.frame("T2H1HDSTD"=as.numeric(dydxdf[[3]]),"D10M"=as.numeric(dydxdf[[4]]))
+  print(df)
+
+  res <- GMM(
+    df,
+    gaussian_comps = 4,
+    dist_mode = "eucl_dist",
+    seed_mode = "random_subset",
+    km_iter = 10,
+    em_iter = 5,
+    verbose = FALSE,
+    var_floor = 1e-10,
+    seed = 1
+  )
+
+  pr = predict(res, newdata = df)
+
+  df$col3 <- pr 
+  print(df)
+  colnames(df) <- c('T2H1HDSTD','D10M','Cluster')
+
+  ggplot(df, aes(x=T2H1HDSTD,y=D10M)) +
+    geom_point(aes(color=factor(Cluster), alpha=0.3)) +
+    #BTC
+    # xlim(-.10,.10) +
+    # ylim(-.10,.10) +
+    theme_tufte() +
+    guides(alpha="none") +
+    scale_color_simpsons() +
+    scale_fill_simpsons() +
+     labs(y="", x="", title="tmp", caption="tmp") +
+    theme(
+      axis.title.x = element_text(color="#e3120b", size=12, face="bold", margin = margin(t = 10, r = 20, b = 0, l = 0)),
+      axis.title.y = element_text(color="#e3120b", size=12, face="bold"),
+      axis.text.x = element_text(angle = 90, vjust = 1)
+      )
 
 #  ggsave(sprintf("%s/%s-%s-%s-%s","clusterdb",i,dumb_hack_1,dumb_hack_2,"dydxmarkets-triple-negvdpv.svg"), width=8, height=6, dpi=300,  units="in")
-ggsave(sprintf("%s","tmp.png"), width=8, height=6, dpi=300,  units="in")
+  ggsave(sprintf("%s","tmp.png"), width=8, height=6, dpi=300,  units="in")
+
+}
+
 
 
 
